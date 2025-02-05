7fecc807c6e7172a8a84acbfde7602bd4a3168d2
Jhey20-sys <gulde.jhey8@gmail.com>
2025-02-05 14:01:25 +0800

    profiles
    ehe

--- src/components/storiesData.ts
+++ src/components/storiesData.ts
@@ -1,11 +1,11 @@
 export const storiesData = [
     { username: "xhaopan", image: "https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg?" },
-    { username: "sheeeisrose", image: "https://mclaren.scene7.com/is/image/mclaren/Placeholder_photo:crop-1x1?wid=700&hei=700/100x100" },
+    { username: "sheeeisrose", image: "https://i.pinimg.com/736x/47/4f/e7/474fe7d5d6d7fb80896589118db1b449.jpg" },
     { username: "lizacali", image: "https://plus.unsplash.com/premium_photo-1667030474693-6d0632f97029?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2F0fGVufDB8fDB8fHww/w=100x100" },
-    { username: "kerbsbual", image: "https://images.ctfassets.net/ub3bwfd53mwy/5WFv6lEUb1e6kWeP06CLXr/acd328417f24786af98b1750d90813de/4_Image.jpg?w=750" },
+    { username: "kerbsbual", image: "https://i.pinimg.com/736x/a9/72/91/a97291c068f1694338d5615902fc85a6.jpg" },
     { username: "pook_kyle", image: "https://images.ctfassets.net/ub3bwfd53mwy/5WFv6lEUb1e6kWeP06CLXr/acd328417f24786af98b1750d90813de/4_Image.jpg?w=750" },
-    { username: "coyenz25", image: "https://images.ctfassets.net/ub3bwfd53mwy/5WFv6lEUb1e6kWeP06CLXr/acd328417f24786af98b1750d90813de/4_Image.jpg?w=750" },
+    { username: "jars_of.clay", image: "https://i.pinimg.com/736x/ec/b4/22/ecb422ab57470d037caf7bc54fd2765c.jpg" },
     { username: "hatdog", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_kucl3OCI2OedQj2lb02ieEscp0IKViVkrBYBOliTyrQNnrsEm_8snZrahcQQXkPS8NU&usqp=CAUw=100x100" },
-    { username: "jheygulde", image: "https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg" },
+    { username: "gei_gurude", image: "https://i.pinimg.com/736x/7e/19/b9/7e19b9c1170a500ddf712745cf9ae172.jpg" },
   ];
   
